ef29618081e81939e5eb9ca4155394fdee2f9815
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-05-09 12:34:31 +0530

    Create Tokenization.py

---
+++ Tokenization.py
@@ -0,0 +1,9 @@
+#Tokenization
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+from nltk import sent_tokenize
+from nltk import word_tokenize
+from nltk.corpus import stopwords
